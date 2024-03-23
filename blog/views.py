bd4797fc29ee82cf81c12ec204809c80fe15c5d9
Mostafa <mostafa.maghool.13811@gmail.com>
2024-03-23 07:44:18 -0800

    Show posts in blog_detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/list.html'
+
+def detail_view(request, blog_id):
+    blog = Post.objects.get(id=blog_id)
+    context={
+        "blog":blog,
+    }
+    return render(request,'blog/detail.html',context)
