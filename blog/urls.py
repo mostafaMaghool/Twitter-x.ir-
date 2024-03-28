147c6ae02b8968fbe0a4ad949f1627739d884a29
Mostafa <mostafa.maghool.13811@gmail.com>
2024-03-28 09:29:58 -0800

    Grow up heaader & login page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(),name='blog_list'),
-    path('<int:blog_id>/', views.detail_view,name='blog_detail'),
+    # path('<int:blog_id>/', views.detail_view,name='blog_detail'),
+    path('<int:pk>/', views.BlogDetailView.as_view(),name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'blog/list.html'
 
-def detail_view(request, blog_id):
-    blog = Post.objects.get(id=blog_id)
-    context={
-        "blog":blog,
-    }
-    return render(request,'blog/detail.html',context)
+# def detail_view(request, blog_id):
+#     blog = Post.objects.get(id=blog_id)
+#     context={
+#         "blog":blog,
+#     }
+#     return render(request,'blog/detail.html',context)
+    
+class BlogDetailView(DeleteView):
+    model = Post
+    template_name = 'blog/detail.html'
